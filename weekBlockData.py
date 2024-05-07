54bc55bb67e1fd27f8911a1612d92e51d7ee1724
Laouni <99427751+Lao0ni@users.noreply.github.com>
2024-05-07 14:00:03 +0200

    chore: typos + deleting cron for block data (now triggered via tenderly)

--- weekBlockData.py
+++ weekBlockData.py
@@ -1,11 +1,12 @@
 import subprocess, json, logging
 from web3 import Web3
 
-'''
+"""
 Fetch block submited on Oracle for week and parse header and data as RLP
-'''
+"""
 
 logging.basicConfig(level=logging.INFO)
+
 
 def load_json(name):
     with open("abi/" + name + ".json", "r") as f:
@@ -13,30 +14,32 @@ def load_json(name):
 
 
 def main():
-    w3 = Web3(Web3.HTTPProvider('https://eth.llamarpc.com'))
+    w3 = Web3(Web3.HTTPProvider("https://eth.llamarpc.com"))
     if w3.isConnected() == False:
         logging.error("RPC down")
         return
 
-
     state_sender_abi = load_json("StateSender")
 
-    # Block hash tooked from the contract, period logic already handled 
-    state_sender = w3.eth.contract(address=Web3.toChecksumAddress("0xC19d317c84e43F93fFeBa146f4f116A6F2B04663"), abi=state_sender_abi)
+    # Block hash tooked from the contract, period logic already handled
+    state_sender = w3.eth.contract(
+        address=Web3.toChecksumAddress("0xC19d317c84e43F93fFeBa146f4f116A6F2B04663"),
+        abi=state_sender_abi,
+    )
 
     # Get current period
     current_period = state_sender.functions.getCurrentPeriod().call()
 
-    # Submit current period to get latest block 
+    # Submit current period to get latest block
     block_number = state_sender.functions.blockNumbers(current_period).call()
 
     # If no block , error
-    if (block_number == 0):
+    if block_number == 0:
         print(f"Error: No block for period {current_period}")
         return
     toji_output = run_toji("https://eth.llamarpc.com", block_number)
     parsed_data = parse_toji_output(toji_output)
-    
+
     # Add block number in a field in the parsed data
     parsed_data["Block Number"] = block_number
     # Add block date (timestamp) in a field in the parsed data
@@ -48,11 +51,9 @@ def main():
 
     logging.info(f"Block {block_number} data: {json_data}")
 
-
     # Write result to a JSON file
     with open("bounties/ethBlockData.json", "w") as file:
         file.write(json_data)
-
 
 
 def parse_toji_output(data):
@@ -77,9 +78,8 @@ def parse_toji_output(data):
 
 
 def to_camel_case(snake_str):
-    components = snake_str.split(' ')
-    return ''.join(x.capitalize() for x in components)
-
+    components = snake_str.split(" ")
+    return "".join(x.capitalize() for x in components)
 
 
 def run_toji(url, block_number):
