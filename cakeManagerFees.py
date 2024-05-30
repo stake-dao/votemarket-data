610c397f2adf206f98b6da9e3493b752f98c5fcc
LucasRamadour <72853747+LucasRamadour@users.noreply.github.com>
2024-05-30 14:00:19 +0200

    fix : request method

--- cakeManagerFees.py
+++ cakeManagerFees.py
@@ -1,7 +1,7 @@
 import requests, json
 
 def main():
-    urlsResponse = requests.get("https://vault.pancakeswap.com/api/statistics?chainId=56&item=feeAvg")
+    urlsResponse = requests.post("https://vault.pancakeswap.com/api/statistics?chainId=56&item=feeAvg", json= {"avgFeeCalculationDays" : 3})
     if urlsResponse.status_code == 200:
         response = urlsResponse.json()
         json_object = json.dumps(response, indent=4)
