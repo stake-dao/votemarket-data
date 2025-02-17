eac0a26b7e1f30d76550f3785037f93e23e7e23f
Marsot Pierre <pierre.mmarsot@gmail.com>
2025-02-17 16:23:44 +0100

    fix

--- cakeTvls.py
+++ cakeTvls.py
@@ -5,11 +5,11 @@ def main():
     f = open("./gauges/cake.json")
     gauges = json.load(f)
     for gauge in gauges["data"] :
-        urlsResponse = requests.get("https://farms-api.pancakeswap.com/v3/"+ str(gauge["chainId"]) + "/liquidity/" + gauge["address"])
-        if urlsResponse.status_code == 200:
-            response = urlsResponse.json()
-            res[gauge["address"]] = response["formatted"]
-
+        if "address" in gauge and "chainId" in gauge:
+            urlsResponse = requests.get("https://farms-api.pancakeswap.com/v3/"+ str(gauge["chainId"]) + "/liquidity/" + gauge["address"])
+            if urlsResponse.status_code == 200:
+                response = urlsResponse.json()
+                res[gauge["address"]] = response["formatted"]
 
     json_object = json.dumps(res, indent=4)
     with open("./tvls/cake.json", "w") as outfile:
